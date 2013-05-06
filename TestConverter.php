6d5e6217d0a4cc48e2ed01079bc57ba90153f1f6
David Manners <dmanners87@gmail.com>
2013-05-06 21:53:36 +0200

    Build a simple validator
    Build a simple validator that calls wolfram alpha and checks if the
    results match

---
+++ TestConverter.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Class TestConverter
+ *  - A class that will call the wolfram alpha api to validate a users conversion
+ *
+ * @author Sitewards <https://github.com/sitewards>
+ */
+class TestConverter {
+	/**
+	 * The App Id from Wolfram Alpha
+	 */
+	const WOLFRAM_ALPHA_APP_ID = 'T6E4XU-GJ39Y2UE2P';
+
+	/**
+	 * The url for the wolfram alpha api
+	 */
+	const WOLFRAM_ALPHA_API_URL = 'http://api.wolframalpha.com/v2/query?appid=%1$s&input=%2$s&format=plaintext&includepodid=%3$s';
+
+	/**
+	 * The pod id for Roman Numerals in wolfram alpha
+	 */
+	const WOLFRAM_ALPHA_POD_ID = 'RomanNumerals';
+
+	/**
+	 * Exception when a connection simplexml_load_file fails
+	 */
+	const EXCEPTION_CONNECTION_ERROR = 'There is an error connecting to wolfram alpha. Please check your internet connection';
+
+	/**
+	 * Exception when response section is missing
+	 */
+	const EXCEPTION_EMPTY_NODE = 'The information provided could not be validated via wolfram alpha';
+
+	/**
+	 * From the given integer
+	 *  - create a url for wolfram alpha
+	 *  - send request
+	 *  - process the node "pod/subpod/plaintext"
+	 *  - validate that this node is the same as the reg_ex result
+	 *
+	 * @param int $iIntegerToBeConverted
+	 * @param string $sRegExResult
+	 * @return bool
+	 * @throws Exception
+	 *  - when there is an error with simplexml_load_file
+	 *  - when there is an error loading the correct node
+	 */
+	public function testConversion($iIntegerToBeConverted, $sRegExResult) {
+		$sWolframAlphaURL = $this->getWolframAlphaUrl($iIntegerToBeConverted);
+
+		$oResponseXml = simplexml_load_file($sWolframAlphaURL);
+		if($oResponseXml === false) {
+			throw new Exception($this::EXCEPTION_CONNECTION_ERROR);
+		}
+
+		$oPodSection = $oResponseXml->pod->subpod->plaintext;
+		if($oPodSection === false) {
+			throw new Exception($this::EXCEPTION_EMPTY_NODE);
+		}
+
+		$sWolframAlphaResponse = (string)$oPodSection;
+
+		if($sWolframAlphaResponse == $sRegExResult) {
+			return true;
+		}
+	}
+
+	/**
+	 * From a mixed input build the wolfram alpha url
+	 *
+	 * @param mixed $mWolframAlphaQuery
+	 * @return string
+	 */
+	private function getWolframAlphaUrl($mWolframAlphaQuery) {
+		return sprintf(
+			$this::WOLFRAM_ALPHA_API_URL,
+			htmlspecialchars($this::WOLFRAM_ALPHA_APP_ID),
+			htmlspecialchars($mWolframAlphaQuery),
+			htmlspecialchars($this::WOLFRAM_ALPHA_POD_ID)
+		);
+	}
+}
--- sample.php
+++ sample.php
@@ -1,7 +1,14 @@
 <?php
 include_once(getcwd().'/Converter.php');
-$oConverter = new Converter();
+include_once(getcwd().'/TestConverter.php');
 
+$oConverter = new Converter();
+$oTestConverter = new TestConverter();
+
+/*
+ * Prompt the user to input a number
+ *  - trim input just to make sure
+ */
 echo "Please enter a number you wish to convert:\n";
 $oHandle = fopen('php://stdin', 'r');
 $mUserInput = trim(fgets($oHandle));
@@ -9,7 +16,12 @@ $mUserInput = trim(fgets($oHandle));
 try {
 	$sRomanNumeral = $oConverter->convertIntegerToRomanNumeral($mUserInput);
 	echo 'Number: ' . htmlspecialchars($mUserInput) . "\n";
-	echo 'Roman Numeral: ' . $sRomanNumeral;
+	echo 'Roman Numeral: ' . $sRomanNumeral . "\n";
+
+	$bValidatedRomanNumeral = $oTestConverter->testConversion($mUserInput, $sRomanNumeral);
+	if($bValidatedRomanNumeral === true) {
+		echo 'Validated at Wolfram Alpha';
+	}
 } catch (Exception $oException) {
 	echo $oException->getMessage();
 }
